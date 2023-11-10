f867e79bbad232a8450fb608772f0ae3c3e34282
Anja <anjakovacevvic@gmail.com>
2023-11-10 13:16:34 +0100

    dlib resen

---
+++ opencv-build/dlib-opencv/main.cpp
@@ -0,0 +1,108 @@
+// #include <iostream>
+// #include <opencv2/highgui.hpp>
+// #include <opencv2/imgcodecs.hpp>
+// #include <opencv2/core.hpp>
+// #include <dlib/opencv.h>
+// #include <dlib/image_processing.h>
+// #include <dlib/image_processing/frontal_face_detector.h>
+// #include <opencv/calib3d/calib3d.hpp>
+// #include <opencv/imgproc/imgproc.hpp>
+
+#include <dlib/opencv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+
+using namespace dlib;
+using namespace std;
+
+int main()
+{
+    // // open cam
+    // cv::VideoCapture cap(0);
+    // dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
+    // if (!cap.isOpened())
+    // {
+    //     std::cout << "Unable to connect to camera" << std::endl;
+    //     return EXIT_FAILURE;
+    // }
+
+    // // main loop
+    // while (1)
+    // {
+    //     // Grab a frame
+    //     cv::Mat temp;
+    //     cap >> temp;
+
+    //     // press esc to end
+    //     cv::imshow("demo", temp);
+    //     unsigned char key = cv::waitKey(1);
+    //     if (key == 27)
+    //     {
+    //         break;
+    //     }
+    // }
+
+    // return 0;
+    try
+    {
+        cv::VideoCapture cap(0);
+        if (!cap.isOpened())
+        {
+            cerr << "Unable to connect to camera" << endl;
+            return 1;
+        }
+
+        image_window win;
+
+        // Load face detection and pose estimation models.
+        frontal_face_detector detector = get_frontal_face_detector();
+        shape_predictor pose_model;
+        deserialize("C:/Users/anja.kovacevic/datasets_and_models/shape_predictor_68_face_landmarks.dat") >> pose_model;
+
+        // Grab and process frames until the main window is closed by the user.
+        while (!win.is_closed())
+        {
+            // Grab a frame
+            cv::Mat temp;
+            if (!cap.read(temp))
+            {
+                break;
+            }
+            // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
+            // wraps the Mat object, it doesn't copy anything.  So cimg is only valid as
+            // long as temp is valid.  Also don't do anything to temp that would cause it
+            // to reallocate the memory which stores the image as that will make cimg
+            // contain dangling pointers.  This basically means you shouldn't modify temp
+            // while using cimg.
+            cv_image<bgr_pixel> cimg(temp);
+
+            // Detect faces
+            std::vector<rectangle> faces = detector(cimg);
+            // Find the pose of each face.
+            std::vector<full_object_detection> shapes;
+            for (unsigned long i = 0; i < faces.size(); ++i)
+                shapes.push_back(pose_model(cimg, faces[i]));
+
+            // Display it all on the screen
+            win.clear_overlay();
+            win.set_image(cimg);
+            win.add_overlay(render_face_detections(shapes));
+            unsigned char key = cv::waitKey(1);
+        }
+    }
+    catch (serialization_error &e)
+    {
+        cout << "You need dlib's default face landmarking model file to run this example." << endl;
+        cout << "You can get it from the following URL: " << endl;
+        cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
+        cout << endl
+             << e.what() << endl;
+    }
+    catch (exception &e)
+    {
+        cout << e.what() << endl;
+    }
+}
