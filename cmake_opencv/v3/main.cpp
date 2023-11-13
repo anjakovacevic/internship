18d9db4e210254514ba305ed873946d55706cf0d
Anja <anjakovacevvic@gmail.com>
2023-11-13 14:09:26 +0100

    v3 added

---
+++ cmake_opencv/v3/main.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/core.hpp>
+// #include <opencv2/imgcodecs.hpp>
+#include <opencv2\imgcodecs\imgcodecs.hpp>
+#include <opencv2\highgui.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened())
+    {
+        cout << "Unable to connect to camera" << endl;
+        return 1;
+    }
+
+    return 0;
+}
